82bea9d37d9820ffa75d8f67b4fa9cfd8adad6c9
chickenCabbage <alon.shiboleth@gmail.com>
2017-11-20 21:33:53 +0200

    added comments

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
-require("dotenv").config({path: "../creds.env"});
+require("dotenv").config({path: "../creds.env"}); //environment variables
 
 var waitingForReply = false 		  ;
-var pesters 		= 0 			  ;
-var patienceTime	= process.argv[2] ;
-var senpai			= process.argv[3] ;
-var subject 		= process.argv[4] ;
-var file 			= process.argv[5] ;
+var pesters 		= 0 			  ; //mail count
+var patienceTime	= process.argv[2] ; //the delay between each email
+var senpai			= process.argv[3] ; //the person being pestered
+var subject 		= process.argv[4] ; //the subject of the email
+var file 			= process.argv[5] ; //the HTML for the message body
 
-var sleep = require("system-sleep"); //for timing to help with the CPU load
+var sleep = require("system-sleep");
 var fs = require("fs");
 
-var nodemailer = require("nodemailer");
-
+var nodemailer = require("nodemailer"); //SMTP API
 function sendMail(recip, subject, content) {
 	var mailOptions = {
-		from 	: process.env.MAILER_USERNAME ,
-		to 		: recip 					  ,
-		subject : subject 					  ,
-		html 	: content
+		from 	: process.env.MAILER_USERNAME , //the sender
+		to 		: recip 					  , //the recipient
+		subject : subject 					  , //the mail subject
+		html 	: content 						//the mail content
 	};
 
 	var transporter = nodemailer.createTransport({
-		service: process.env.MAILER_SERVICE,
+		service    : process.env.MAILER_SERVICE  , //nodemailer recongnizes most email companies by their name
 		auth: {
-			"user" : process.env.MAILER_USERNAME,
-			"pass" : process.env.MAILER_PASSWORD
+			"user" : process.env.MAILER_USERNAME , //login username
+			"pass" : process.env.MAILER_PASSWORD   //login password
 		}
 	});
 	transporter.sendMail(mailOptions, function(error, info){
-		if(error) console.log("error in mailing!\n" + error);
+		//if(error) throw error; //redundant, errors throw themselves
+		console.log("Email successfully sent.");
+		pesters ++;
 	});
-	pesters ++;
 }//end sendMail()
 
 console.log("SMTP prepared as " + process.env.MAILER_USERNAME + ".");
 
-var MailListener = require("mail-listener2"); //email API
+var MailListener = require("mail-listener2"); //IMAP API
 var mailListener = new MailListener({
-	username : process.env.EMAIL_LISTENER_USERNAME ,
-	password : process.env.LISTENER_PASSWORD 	   , //log in
-	host 	 : process.env.EMAIL_LISTENER_HOST 	   ,
-	port 	 : process.env.EMAIL_IMAP_PORT 		   ,
-	tls 	 : process.env.EMAIL_IMAP_TLS 		   ,
-	mailbox  : "INBOX" 							   ,
-	markSeen : false
+	username : process.env.EMAIL_LISTENER_USERNAME , //login username
+	password : process.env.LISTENER_PASSWORD 	   , //login password
+	host 	 : process.env.EMAIL_LISTENER_HOST 	   , //the IMAP host address
+	port 	 : process.env.EMAIL_IMAP_PORT 		   , //IMAP port, usually not needed
+	tls 	 : process.env.EMAIL_IMAP_TLS 		   , //allow TLS
+	mailbox  : "INBOX" 							   , //if there are multiple inboxes, such as spam
+	markSeen : false 								 //automatic "seen" marking on mail open
 });
 
- mailListener.on("server:connected", function() {
+ mailListener.on("server:connected", function() { //when IMAP connects
 	console.log("IMAP is ready as " + process.env.EMAIL_LISTENER_USERNAME + ".");
 });
-mailListener.on("server:disconnected", function() {
+mailListener.on("server:disconnected", function() { //if IMAP disconnects
 	console.log("Disconnected from email!");
-	mailListener.stop()
+	mailListener.stop() //officially stop
 
-	if(waitingForReply) {
+	if(waitingForReply) { 	  //just to make sure 
 		mailListener.start(); //reconnect
 		console.log("Restarted email connection.");
 	}
 });
 
-mailListener.on("mail", function(mail, seqno, attributes) {
+mailListener.on("mail", function(mail, seqno, attributes) { //when an email has been recieved
 	console.log("Mail recieved!");
 	var addr = mail.from[0].address; //isolate the email address
 	if(addr == senpai) {
@@ -73,13 +73,12 @@ mailListener.on("mail", function(mail, seqno, attributes) {
 			" but hey, someone wanted to talk to you" ,
 			", and you are so, so lonely."
 		];
-		console.log("It wasn't senpai" + insults[Math.floor(Math.random() * insults.length)]);
+		console.log("It wasn't senpai" + insults[Math.floor(Math.random() * insults.length)]); // :^(
 	}
 });
 
 async function timer() {
-	console.log("Timer has started.");
-	while(true) {
+	while(true) {  						 //until process.exit(0)
 		sleep(patienceTime * 60 * 1000); //minutes to milis
 		sendMail(senpai, subject, fs.readFileSync(file));
 		console.log("mail sent");
